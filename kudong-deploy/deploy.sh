aa1e5bf1fb877476106768d863177505d9746668
구동혁 <dhku@naver.com>
2024-04-02 20:34:53 +0900

    deploy 수정

--- kudong-deploy/deploy.sh
+++ kudong-deploy/deploy.sh
@@ -68,8 +68,8 @@ do
     echo "Start Mount....."
     mkdir -p $mountDir;
    
-    sudo sshfs $id@$ip:$hostBaseDir $mountDir;
-    #echo "${hostPassword}" | sshfs $id@$ip:$hostBaseDir $mountDir -o workaround=rename -o password_stdin -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+    #sudo sshfs $id@$ip:$hostBaseDir $mountDir;
+    echo "${hostPassword}" | sshfs $id@$ip:$hostBaseDir $mountDir -o workaround=rename -o password_stdin -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
     #sshfs -o password_stdin $id@$ip:$hostBaseDir $mountDir <<< "${hostPassword}"
     echo "$id@$ip:$hostBaseDir $mountDir";
     mountedDirList["$hostname"]="$mountDir";
