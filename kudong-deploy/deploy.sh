de33c498dce3dd280e0af6f3644fb84ffb86c7a3
구동혁 <dhku@naver.com>
2024-04-02 22:51:42 +0900

    수정

--- kudong-deploy/deploy.sh
+++ kudong-deploy/deploy.sh
@@ -105,7 +105,7 @@ do
 	while true;
 	do
         echo "Test2"
-        hostname=${minecraftList["$screenName"]}
+        hostname="${minecraftList["$screenName"]}"
 
         echo "hostname = $hostname"
 
