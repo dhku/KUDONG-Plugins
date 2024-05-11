922403cd05e85ec75565bf6cc97d6c22ff45e6de
구동혁 <dhku@naver.com>
2024-05-11 22:05:46 +0900

    수정

--- kudong-common/src/main/java/kr/kudong/common/basic/config/ConfigurationMember.java
+++
@@ -1,9 +0,0 @@
-package kr.kudong.common.basic.config;
-
-import java.util.Map;
-
-public interface ConfigurationMember
-{
-	public boolean installConfig(Map<String, Object> config);
-	public Map<String, Object> getModuleConfig();
-}
